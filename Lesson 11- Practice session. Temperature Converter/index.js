5aae0399f615355054be1d3a84a875191c8c3493
Salah Uddin @Sweden <77015008+SalahUddinSweden@users.noreply.github.com>
2021-04-29 02:01:56 +0200

    Add files via upload

---
+++ Lesson 11- Practice session. Temperature Converter/index.js
@@ -0,0 +1,11 @@
+
+//Temperature Converter Fahrenheit to Celsius
+
+
+var Fahrenheit = parseFloat(prompt('What is Todays Temperature in Fahrenheit :'));
+
+var Celsius = (Fahrenheit -32) * (5/9);
+
+
+document.write('TODAY TEMPERATURE in CELSIUS is = ' + Celsius + ' Degree');
+
